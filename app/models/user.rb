acb63dfe2e8615cc98e56dd6148dce1ac6c237c2
TeatimeinOrbit <108617356+TeatimeinOrbit@users.noreplay.github.com>
2022-08-06 14:33:10 +0000

    [Finish_Update4]:フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :follower, class_name: Relationship, foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: Relationship, foreign_key: "followed_id", dependent: :destroy
-  has_many :following_user, through: :follower, source: :followed
-  has_many :follower_user, through: :followed, source: :follower
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :follower, source: :followed
+  has_many :followers, through: :followed, source: :follower
 
   def follow(user_id)
     follower.create(followed_id: user_id)
@@ -21,7 +22,7 @@ class User < ApplicationRecord
   end
 
   def following?(user)
-    following_user.include?(user)
+    followings.include?(user)
   end
 
   has_one_attached :profile_image
